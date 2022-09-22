338673487294d0f02206da41c8e759f29e321333
Deepak <deepaknnayak34@gmail.com>
2022-09-23 01:36:03 +0530

    added programs in while Loop

--- question.py
+++ question.py
@@ -1,79 +1,148 @@
-#TODO: 1. Write a python script to print MySirG N times on the screen
-n = int(input('Enter the value of n:'))
-for i in range(n):
-  print("MySirG")
+# #TODO: 1. Write a python script to print MySirG N times on the screen
+# n = int(input('Enter the value of n:'))
+# for i in range(n):
+#   print("MySirG")
 
+# TODO: another way
+# for i in range(int(input('Enter The value of n:'))):
+#   print('MySirG')
 
-# TODO: Write a python script to print first N natural numbers.
-n = int(input('Enter the value of n:'))
-for i in range(n+1):
-  if i == 0:
-    continue
-  else:
-    print(i)
+# TODO:using While Loop
+# n = int(input('Enter the value of n:'))
+# i=0
+# while i<n:
+#   print('MySirG')
+#   i=i+1
 
+# # TODO: Write a python script to print first N natural numbers.
+# n = int(input('Enter the value of n:'))
+# for i in range(n+1):
+#   if i == 0:
+#     continue
+#   else:
+#     print(i)
 
+# TODO: While lOOP
+# n = int(input('Enter the value of n:'))
+# i=1
+# while i <=n:
+#   print(i)
+#   i=i+1
 
-#TODO: 3. Write a python script to print first N natural numbers in reverse order
-n = int(input('Enter the value of n:'))
-for i in range(n,0,-1):
-  print(i)
-
-
-# TODO: Write a python script to print first N odd natural numbers
-n = int(input('Enter the value of n:'))
-for i in range(n*2):
-  if i % 2 !=0:
-    print(i)
-
-# TODO: Another way
-# n = int(input('Enter the value of n:'))*2
-# for i in range(1,n,2):
+# #TODO: 3. Write a python script to print first N natural numbers in reverse order
+# n = int(input('Enter the value of n:'))
+# for i in range(n,0,-1):
 #   print(i)
 
-# TODO:5. Write a python script to print first N odd natural numbers in reverse order
-n = int(input('Enter the value of n:'))*2
-for i in range(n):
-  if i % 2 !=0:
-    print(n-i)
+# n = int(input('Enter the number:'))
+# i=n
+# while i>=1:
+#   print(i)
+#   i=i-1
 
 
+# # TODO: Write a python script to print first N odd natural numbers
+# n = int(input('Enter the value of n:'))
+# for i in range(n*2):
+#   if i % 2 !=0:
+#     print(i)
 
-# TODO: 6. Write a python script to print first N even natural numbers
+# # TODO: Another way
+# # n = int(input('Enter the value of n:'))*2
+# # for i in range(1,n,2):
+# #   print(i)
 
-n = int(input('Enter the value of n:'))*2
-for i in range(2,n+2,2):
-  print(i)
+# TODO: While lOOP
+# n = int(input('Enter the value of n:'))
+# i=1
+# while i<=n*2:
+#   print(i)
+#   i+=2
 
-# TODO: Another way
-n = int(input('Enter the value of n:'))*2
-for i in range(2,n+1):
-  if i % 2 == 0:
-    print(i)
+# # TODO:5. Write a python script to print first N odd natural numbers in reverse order
+# n = int(input('Enter the value of n:'))*2
+# for i in range(n):
+#   if i % 2 !=0:
+#     print(n-i)
+
+# TODO: While lOOP
+# n = int(input('Enter the value of n:'))
+# i=n*2-1
+# while i>=1:
+#   print(i)
+#   i=i-2
+
+
+# # TODO: 6. Write a python script to print first N even natural numbers
+
+# n = int(input('Enter the value of n:'))*2
+# for i in range(2,n+2,2):
+#   print(i)
+
+# # TODO: Another way
+# n = int(input('Enter the value of n:'))*2
+# for i in range(2,n+1):
+#   if i % 2 == 0:
+#     print(i)
+
+# TODO: While lOOP
+# n = int(input('Enter the  number:'))
+# i=2
+# while i<=n+n:
+# while i<=n*2
+#   print(i)
+#   i+=2
+
   
 
-# TODO: 7. Write a python script to print first N even natural numbers in reverse order
-n = int(input('Enter the value of n:'))*2
-for i in range(0,n):
-  if i % 2 ==0:
-    print(n-i)
+# # TODO: 7. Write a python script to print first N even natural numbers in reverse order
+# n = int(input('Enter the value of n:'))*2
+# for i in range(0,n):
+#   if i % 2 ==0:
+#     print(n-i)
+
+# TODO: While Loop
+# n = int(input('Enter the value of n:'))
+# i=n*2
+# while i>=2:
+#   print(i)
+#   i-=2
+
+# #TODO:8. Write a python script to print squares of first N natural numbers
+
+# n = int(input('Enter the value of n:'))
+# for i in range(1,n+1):
+#   print(i**2)
+
+# TODO: While lOOP
+# n = int(input('Enter the value of n:'))
+# i=1
+# while i<n:
+#   print(i**2)
+#   i=i+1
 
 
-#TODO:8. Write a python script to print squares of first N natural numbers
+# # TODO:9. Write a python script to print cubes of first N natural numbers
 
+# n = int(input('Enter the value of n:'))
+# for i in range(1,n+1):
+#   print(i**3)
+
+# TODO: While Loop
+# n = int(input('Enter the value of n:'))
+# i=1
+# while i<=n:
+#   print(i**3)
+#   i+=1
+
+# #TODO: 10. Write a python script to print first 10 multiples of Nq
+# n = int(input('Enter the number:'))
+# for i in range(1,n+1):
+#   print(i*10)
+
+# TODO: While Loop
 n = int(input('Enter the value of n:'))
-for i in range(1,n+1):
-  print(i**2)
-
-
-# TODO:9. Write a python script to print cubes of first N natural numbers
-
-n = int(input('Enter the value of n:'))
-for i in range(1,n+1):
-  print(i**3)
-
-
-#TODO: 10. Write a python script to print first 10 multiples of Nq
-n = int(input('Enter the number:'))
-for i in range(1,n+1):
-  print(i*10)
+i=1
+while i<=n:
+  print(i,":",i*10)
+  i+=1
